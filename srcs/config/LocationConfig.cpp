dceb2fd4ba0e28dd5b48cef6e2ffa5a6efec829e
seipan1213 <seiju1213@gmail.com>
2022-10-02 17:23:28 +0900

    LocationConfig 仮実装 #2

---
+++ srcs/config/LocationConfig.cpp
@@ -0,0 +1,92 @@
+#include "LocationConfig.hpp"
+
+LocationConfig::LocationConfig()
+	: alias(), autoindex(), allow_method(),
+	  index(), cgi_extension(), redirect(),
+	  error_page(), upload_filepath()
+{
+}
+
+LocationConfig::~LocationConfig()
+{
+}
+
+void LocationConfig::setAlias(const std::string &alias)
+{
+	this->alias = alias;
+}
+
+void LocationConfig::setAutoindex(const bool autoindex)
+{
+	this->autoindex = autoindex;
+}
+
+void LocationConfig::addAllowMethod(const std::string &allow_method)
+{
+	this->allow_method.push_back(allow_method);
+}
+
+void LocationConfig::addIndex(const std::string &index)
+{
+	this->index.push_back(index);
+}
+
+void LocationConfig::addCgiExtension(const std::string &cgi_extension)
+{
+	this->cgi_extension.push_back(cgi_extension);
+}
+
+void LocationConfig::addRedirect(const int redirect_status, const std::string &uri)
+{
+	this->redirect.insert(std::make_pair(redirect_status, uri));
+}
+
+void LocationConfig::addErrorPage(const int error_status, const std::string &uri)
+{
+	this->error_page.insert(std::make_pair(error_status, uri));
+}
+
+void LocationConfig::setUploadFilepath(const std::string &upload_filepath)
+{
+	this->upload_filepath = upload_filepath;
+}
+
+const std::string &LocationConfig::getAlias() const
+{
+	return this->alias;
+}
+
+const bool &LocationConfig::getAutoIndex() const
+{
+	return this->autoindex;
+}
+
+const std::vector<std::string> &LocationConfig::getAllowMethod() const
+{
+	return this->allow_method;
+}
+
+const std::vector<std::string> &LocationConfig::getIndex() const
+{
+	return this->index;
+}
+
+const std::vector<std::string> &LocationConfig::getCgiExtension() const
+{
+	return this->cgi_extension;
+}
+
+const std::map<int, std::string> &LocationConfig::getRedirect() const
+{
+	return this->redirect;
+}
+
+const std::map<int, std::string> &LocationConfig::getErrorPage() const
+{
+	return this->error_page;
+}
+
+const std::string &LocationConfig::getUploadFilepath() const
+{
+	return this->upload_filepath;
+}
---
+++ srcs/config/LocationConfig.hpp
@@ -0,0 +1,43 @@
+#ifndef LOCATION_CONFIG_HPP
+#define LOCATION_CONFIG_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+class LocationConfig
+{
+private:
+	std::string alias;
+	bool autoindex;
+	std::vector<std::string> allow_method;
+	std::vector<std::string> index;
+	std::vector<std::string> cgi_extension;
+	std::map<int, std::string> redirect;
+	std::map<int, std::string> error_page;
+	std::string upload_filepath;
+
+public:
+	LocationConfig();
+	~LocationConfig();
+
+	void setAlias(const std::string &alias);
+	void setAutoindex(const bool autoindex);
+	void addAllowMethod(const std::string &allow_method);
+	void addIndex(const std::string &index);
+	void addCgiExtension(const std::string &cgi_extension);
+	void addRedirect(const int redirect_status, const std::string &uri);
+	void addErrorPage(const int error_status, const std::string &uri);
+	void setUploadFilepath(const std::string &upload_filepath);
+
+	const std::string &getAlias() const;
+	const bool &getAutoIndex() const;
+	const std::vector<std::string> &getAllowMethod() const;
+	const std::vector<std::string> &getIndex() const;
+	const std::vector<std::string> &getCgiExtension() const;
+	const std::map<int, std::string> &getRedirect() const;
+	const std::map<int, std::string> &getErrorPage() const;
+	const std::string &getUploadFilepath() const;
+};
+
+#endif
