d692361b54bb5e15e4e8c4011adad00a977aeaa2
AuroraSloan <lanajadethompson@gmail.com>
2022-10-05 03:55:21 +0900

    start parsing

--- HTTPRequest.cpp
+++ HTTPRequest.cpp
@@ -4,10 +4,51 @@ HTTPRequest::HTTPRequest(){}
 HTTPRequest::~HTTPRequest(){}
 
 void                HTTPRequest::ParseHTTPRequest(const std::string& request) {
+    // Need to handle request line??
+    std::stringstream   ss(reqeust);
+    std::string rqst = std::getline(ss, rqst, '\n');
 
+    _parseStartLine(rqst);
+    while (std::getline(ss, rqst, '\n')) {
+        _parseHeaderFields(ss, rqst);
+    }
+    // if !content-length
+    //    retrn
+    // else
+    //    getbody
 }
 
-const std::string&  HTTPRequest::GetRequestMethod() { return RequestMethod; }
-const std::string&  HTTPRequest::GetRequestURI() { return RequestURI; }
-const header_type&  HTTPRequest::GetHeaders() { return Headers; }
-const std::string&  HTTPRequest::GetBody() { return Body; }
+const std::string&  HTTPRequest::GetRequestMethod() { return _requestMethod; }
+const std::string&  HTTPRequest::GetRequestURI() { return _requestURI; }
+const header_type&  HTTPRequest::GetHeaderFields() { return _headerFields; }
+const std::string&  HTTPRequest::GetBody() { return _body; }
+
+size_t  HTTPRequest::_parseStartLine(const std::string& request) {
+    size_t  = 0;
+
+    // get Request Method
+    i = request.find_first_of(' ');
+    if (i != std::string::npos) {
+        _requestMethod = request.substr(0, i);
+    } else {;}//Throw exception?なんかエラーハンドル
+    start = i + 1;
+
+    // get request-URI
+    i = request.find_last_of(' ');
+    if (i != std::string::npos) {
+        _requestURI = request.substr(start, i - start);
+    } else {;}//Throw exception?なんかエラーハンドル
+    start = i + 1;
+
+    i = request.find('\n');
+    if (request.substr(start, i - start) != "HTTP/1.1"
+        || !_requestMethod || !_requestURI) {
+           ;//Throw exception?なんかエラーハンドル
+    }
+}
+
+void    _parseHeaderFields(const std::stringstream& ss, const std::string& request) {
+    while (std::getline(ss, rqst, '\n') /*!= linebreak*/) {
+        // parse eacher header
+    }
+}
--- HTTPRequst.hpp
+++ HTTPRequst.hpp
@@ -3,26 +3,33 @@
 # define HTTPREQUEST_HPP
 
 #include <string>
+#include <cstddef>
 
 class HTTPRequest {
 public:
     typdef std::vector<std::pair<std::string, std::string> >   header_type;
 
 private:
-    std::string     RequestMethod;
-    std::string     RequestURI;
-    header_type     Headers;
-    std::string     Body;
+    std::string     _requestMethod;
+    std::string     _requestURI;
+    header_type     _headerFields;
+    std::string     _body;
 
 public:
     HTTPRequest(){}
     ~HTTPRequest(){}
 
     void ParseHTTPRequest(const std::string& request);
+
     const std::string&  GetRequestMethod();
     const std::string&  GetRequestURI();
-    const header_type&  GetHeaders();
+    const header_type&  GetHeaderFields();
     const std::string&  GetBody();
+
+private:
+
+    void    _parseStartLine(const std::string& request);
+    void    _parseHeaderFields(const std::stringstream& ss, const std::string& request);
 
 };
 
