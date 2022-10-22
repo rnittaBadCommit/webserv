8bf26ff8d981d816d1e1207c8fedb57661305a7f
ymori <yuu.mori06@gmail.com>
2022-10-22 20:02:55 +0900

    feat: add do_delete()

--- srcs/httpMethod/HTTPMethod.cpp
+++ srcs/httpMethod/HTTPMethod.cpp
@@ -34,70 +34,6 @@ typedef std::map<std::string, std::string> http_header_t;
  * @return
  *  status code
 */
-int executeGet() {
-  // Temporary valuables
-  std::string path = "/home";
-  std::string location;
-  std::string cgi;
-
-  // Check PATH
-  if (!path.empty()) {
-    location = path;
-  } else {
-    // TODO: send Error message "Location not found" to client
-    return 404;
-  }
-
-  // Skip Auth
-
-  // TODO: Index search
-  // <uri>?<param>&<param>&...
-
-  std::string index = "ok";
-
-  if (!index.empty()) {
-
-  }
-
-  /*
-   * Check and judge status
-   *
-   * If-Modified-Since
-   * If-Unmodified-Since
-   * If-Match
-   * If-None-Match
-   * If-Range
-   *
-   * 304
-   * 412
-   * 206
-   * 200
-   * 206
-   * 200
-   */
-
-  /*
-   * Response Header
-   */
-
-  std::string method = "GET";
-  std::string type = "text";
-
-  // send entity
-  if (method == "GET") {
-    if (type == "binary") {
-      /*
-       * TODO: sentBinary in Socket class
-       * sendBinary(path)
-       */
-    } else {
-      /*
-       * TODO: sendText in Socket class
-       * sendText(path)
-       */
-    }
-  }
-}
 
 std::string CreateErrorSentence(int status_code) {
   std::stringstream error_sentence;
@@ -116,9 +52,99 @@ std::string CreateErrorSentence(int status_code) {
   error_sentence << "HTTP/1.1 " << HttpResponse::GetResponseLine(status_code)
                  << CRLF; // TODO: check status code
   error_sentence << "Server: " << "42webserv" << "/1.0" << CRLF;
+  error_sentence << "Date: " << date << CRLF;
   error_sentence << "Content-Type: text/html" << CRLF;
 
   return error_sentence.str();
+}
+
+int do_get(http_header_t http_header,
+           std::string &file_path,
+           std::string &response_message_str) {
+  int response_status;
+  std::stringstream response_message_stream;
+
+  int ret_val;
+
+  if (!file_path.empty()) {
+    // TODO: index search
+    int ret_val = 0;
+    if (ret_val != 0) { // Resource not found
+      response_message_stream << CreateErrorSentence(404);
+      return (404);
+    }
+    // copy file path to path ?
+  }
+  // ret_val = stat
+  if (ret_val < 0) {
+    response_message_stream << CreateErrorSentence(404);
+    return (404);
+  }
+
+  // Check Mod, Match, Range and each processing
+  /*
+   * Check and judge status
+   *
+   * If-Modified-Since
+   * If-Unmodified-Since
+   * If-Match
+   * If-None-Match
+   * If-Range
+   *
+   * 304
+   * 412
+   * 206
+   * 200
+   * 206
+   * 200
+   */
+
+  response_message_stream << "HTTP/1.1 200 OK" << CRLF;
+  response_status = 200;
+
+  // Server:
+  response_message_stream << "Server: " << "42webserv" << "/1.0" << CRLF;
+
+  // Date:
+  char date[1024];
+  time_t gmt_time;
+  time(&gmt_time);
+  strftime(date, 1024, "%a, %d %b %Y %X %Z", gmtime(&gmt_time)); // RFC7231
+  response_message_stream << "Date: " << date << CRLF;
+
+  // Last-Modified:
+  strftime(date, 1024, "%a, %d %b %Y %X %Z", gmtime(&gmt_time)); // RFC7231
+  response_message_stream << "Last-Modified: " << date << CRLF;
+
+  // Force search results to text/html type
+  if (!file_path.empty()) {
+    // find type "x.html"
+  } else {
+    // find type file_path
+  }
+
+  // sending partial content
+
+  // send full entry
+  // Content-Type:
+  // Content-Length:
+
+  // don't send unless GET
+  if (method == "GET") {
+    if (type == "binary") {
+      /*
+       * TODO: sentBinary in Socket class
+       * sendBinary(path)
+       */
+    } else {
+      /*
+       * TODO: sendText in Socket class
+       * sendText(path)
+       */
+    }
+  }
+
+  return response_status;
 }
 
 int do_delete(http_header_t http_header,
@@ -153,6 +179,7 @@ int do_delete(http_header_t http_header,
   time_t gmt_time;
   time(&gmt_time);
   strftime(date, 1024, "%a, %d %b %Y %X %Z", gmtime(&gmt_time)); // RFC7231
+  response_message_stream << "Date: " << date << CRLF;
 
   response_message_str = response_message_stream.str();
   return response_status;
