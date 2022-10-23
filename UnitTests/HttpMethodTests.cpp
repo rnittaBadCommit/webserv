c233e63ea9d00a48fdb954f9b1f4a43d844233b2
ymori <yuu.mori06@gmail.com>
2022-10-24 02:09:57 +0900

    feat: add minimum do_get tests #16

--- UnitTests/HttpMethodTests.cpp
+++ UnitTests/HttpMethodTests.cpp
@@ -6,24 +6,61 @@
 #include "gtest/gtest.h"
 #include "../srcs/httpMethod/HTTPMethod.hpp"
 #include "../srcs/httpMethod/MethodUtils.hpp"
+#include "../srcs/HTTP/HTTPRequst.hpp"
 
 namespace DoGet{
  class DoGet : public ::testing::Test {
   protected:
-   void SetUp() override {
-
+   static void SetUpTestCase() {
+     // First function only
+     chdir("../.."); // Go to the project root directory
    }
+
+   void SetUp() override {
+     file_path = "UnitTests/testroot/var/www/index.html";
+     dir_path = "UnitTests/testroot/var/www/do_not_delete";
+     wrong_path = "UnitTests/testroot/var/www/fdsafdasf.html";
+   }
+
+   std::string file_path;
+   std::string dir_path;
+   std::string wrong_path;
  };
 
-TEST_F(DoGet, PathEmpty) {
+TEST_F(DoGet, SuccessCase1) {
   std::string path;
-  http_header_t http_header;
+  ft::HTTPRequest http_request("10000");
   std::string response_message_str;
 
-  do_get(http_header, path, response_message_str);
+  EXPECT_EQ(do_get(http_request, file_path, response_message_str), 200);
   std::cout << "Server Error*****" << std::endl;
   std::cout << response_message_str << std::endl;
   std::cout << "*****" << std::endl;
+
+}
+
+TEST_F(DoGet, Case404_wrong_path) {
+  std::string path;
+  ft::HTTPRequest http_request("10000");
+  std::string response_message_str;
+
+  EXPECT_EQ(do_get(http_request, wrong_path, response_message_str), 404);
+  std::cout << "Server Error*****" << std::endl;
+  std::cout << response_message_str << std::endl;
+  std::cout << "*****" << std::endl;
+
+}
+
+TEST_F(DoGet, Case404_dir_path) {
+  std::string path;
+  ft::HTTPRequest http_request("10000");
+  std::string response_message_str;
+
+  EXPECT_EQ(do_get(http_request, dir_path, response_message_str), 404);
+  std::cout << "Server Error*****" << std::endl;
+  std::cout << response_message_str << std::endl;
+  std::cout << "*****" << std::endl;
+
 }
 
 }
@@ -31,57 +68,42 @@ TEST_F(DoGet, PathEmpty) {
 namespace DoDelete{
  class DoDelete : public ::testing::Test {
   protected:
-   void SetUp() override {
-     ok_path = "/tmp/www/index.html";
+   static void SetUpTestCase() {
+     // First function only
+     chdir("../.."); // Go to the project root directory
    }
 
-   std::string ok_path;
+   void SetUp() override {
+     delete_test_file_path = "UnitTests/testroot/var/www/delete_test_file.html";
+     wrong_path = "UnitTests/testroot/var/www/fdsafdasf.html";
+   }
+
+   std::string delete_test_file_path;
+   std::string wrong_path;
  };
 
 TEST_F(DoDelete, ServerError500) {
   http_header_t http_header;
   std::string response_message_str;
 
-  EXPECT_EQ(do_delete(http_header, ok_path, response_message_str), 501);
+
+  EXPECT_EQ(do_delete(http_header, wrong_path, response_message_str), 500);
   std::cout << "Server Error*****" << std::endl;
   std::cout << response_message_str << std::endl;
   std::cout << "*****" << std::endl;
 }
 
-TEST_F(DoDelete, ServerError501) {
-  http_header_t http_header {
-      {"Range :", "100"}
-  };
-  std::string response_message_str;
-
-  EXPECT_EQ(do_delete(http_header, ok_path, response_message_str), 500);
-  std::cout << "Server Error*****" << std::endl;
-  std::cout << response_message_str << std::endl;
-  std::cout << "*****" << std::endl;
-}
 
 TEST_F(DoDelete, SuccessCase) {
-  http_header_t http_header {
-      {"Range :", "100"}
-  };
+  http_header_t http_header;
   std::string response_message_str;
 
-//  char tmp[256];
-//  getcwd(tmp, 256);
-//  std::cout << "pwd: " << tmp << std::endl;
-//
-  chdir("../..");
-//  getcwd(tmp, 256);
-//  std::cout << "pwd: " << tmp << std::endl;
-
-  std::string test_file_path = "UnitTests/testroot/var/www/delete_test_file.html";
-
   std::ofstream delete_test_file;
-  delete_test_file.open(test_file_path);
+  delete_test_file.open(delete_test_file_path);
   delete_test_file << "TEST" << std::endl;
   delete_test_file.close();
 
-  EXPECT_EQ(do_delete(http_header, test_file_path, response_message_str), 204);
+  EXPECT_EQ(do_delete(http_header, delete_test_file_path, response_message_str), 204);
   std::cout << "Server Error*****" << std::endl;
   std::cout << response_message_str << std::endl;
   std::cout << "*****" << std::endl;
