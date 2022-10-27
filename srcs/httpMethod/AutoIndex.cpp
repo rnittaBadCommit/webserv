f1923fc4a52f186ea0579249e0f7b3820cdf81b4
ymori <yuu.mori06@gmail.com>
2022-10-27 23:24:42 +0900

    update: AutoIndex #16

--- srcs/httpMethod/AutoIndex.cpp
+++ srcs/httpMethod/AutoIndex.cpp
@@ -13,50 +13,55 @@
 std::string get_table_line(std::string path) {
   std::stringstream return_sentence;
 
-  return_sentence << "  <tr>" CRLF
+  return_sentence << " <tbody>" CRLF
+                  << "  <tr>" CRLF
                   << "   <td>" CRLF
                   << "    " << path << CRLF
-                  << "   </td>" CRLF
-                  << "   <td>" CRLF
-                  << "    " << "size" << CRLF
-                  << "   </td>" CRLF
-                  << "   <td>" CRLF
-                  << "    " << "last modified" << CRLF
-                  << "   </td>" CRLF
-                  << "  </tr>" CRLF;
+                  << "   </td>" CRLF;
+//                  << "   <td>" CRLF
+//                  << "    " << "last modified" << CRLF
+//                  << "   </td>" CRLF
+//                  << "   <td>" CRLF
+//                  << "    " << "size" << CRLF
+//                  << "   </td>" CRLF
+//                  << "  </tr>" CRLF;
+  return_sentence << " </tbody>"  CRLF;
 
   return return_sentence.str();
 }
 
-std::string render_html(std::set<std::string> dirpath_list) {
+std::string render_html(std::string dirpath_root, std::set<std::string> dirpath_list) {
   std::stringstream return_sentence;
 
   return_sentence << "<html>" CRLF
                   << "<head>" CRLF
                   << " <title>" CRLF
-                  << "TODO: TITLE" CRLF // TODO: title
+                  << "Auto index" CRLF
                   << " </title>" CRLF
                   << "</head>" CRLF;
 
   return_sentence << "<body>" CRLF
-                  << "<h2>Index of " << "TODO: ROOT DIRPATH" << "</h2>" CRLF;
+                  << "<h2>Index of " << dirpath_root << "</h2>" CRLF;
 
   return_sentence << "<hr>"  CRLF
                   << "<table>" CRLF
                   << " <thead>" CRLF
                   << "  <tr>" CRLF
                   << "   <th>Name</th>" CRLF
-                  << "   <th>Last Modified</th>" CRLF
-                  << "   <th>Size</th>" CRLF
+//                  << "   <th>Last modified</th>" CRLF
+//                  << "   <th>Size</th>" CRLF
                   << "  </tr>" CRLF
-                  << " </thead>" CRLF
-                  << " <tbody>" CRLF;
+                  << " </thead>" CRLF;
+
   // get table line
   for (std::set<std::string>::iterator iter = dirpath_list.begin(); iter != dirpath_list.end(); ++iter) {
+    // "./" is not printed
+    if (*iter == "./")
+      continue;
     return_sentence << get_table_line(*iter);
   }
-  return_sentence << " </tbody>"  CRLF
-                  << "</table>"  CRLF
+
+  return_sentence << "</table>"  CRLF
                   << "</body>"  CRLF
                   << "</html>"  CRLF;
 
--- srcs/httpMethod/AutoIndex.hpp
+++ srcs/httpMethod/AutoIndex.hpp
@@ -5,4 +5,6 @@
 #ifndef WEBSERV_SRCS_HTTPMETHOD_AUTOINDEX_HPP_
 #define WEBSERV_SRCS_HTTPMETHOD_AUTOINDEX_HPP_
 
+std::string render_html(std::string dirpath_root, std::set<std::string> dirpath_list);
+
 #endif //WEBSERV_SRCS_HTTPMETHOD_AUTOINDEX_HPP_
