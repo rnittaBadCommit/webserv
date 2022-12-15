e6df2b0b026bf7eb56fe7c140df7e9bd662538b8
AuroraSloan <lanajadethompson@gmail.com>
2022-12-16 01:39:22 +0900

    init

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CXXFLAGS := -std=c++98 -pedantic -Wall -Wextra -Werror
 CONFIGSRCS := srcs/config/Config.cpp srcs/config/ConfigParser.cpp srcs/config/LocationConfig.cpp srcs/config/ServerConfig.cpp
 HTTPSRCS := srcs/HTTP/HTTPHead.cpp
 SOCKETSRCS := srcs/server/Cgi.cpp srcs/server/server.cpp srcs/server/serverChild.cpp srcs/server/socket.cpp #srcs/server/URL_to_Path.cpp
-SRCS := main.cpp srcs/utils.cpp $(CONFIGSRCS) $(HTTPSRCS) $(SOCKETSRCS)
+SRCS := main.cpp srcs/utils/utils.cpp $(CONFIGSRCS) $(HTTPSRCS) $(SOCKETSRCS)
 OBJS := $(SRCS:.cpp=.o)
 
 $(NAME): $(OBJS)
--- UnitTests/HttpRequestTests.cpp
+++ UnitTests/HttpRequestTests.cpp
@@ -6,39 +6,81 @@
 #include "../srcs/HTTP/HTTPHead.hpp"
 
 namespace {
- class HTTPRequestTest : public ::testing::Test {
+    class HTTPRequestTest : public ::testing::Test {
 
- };
+    };
 
-TEST_F(HTTPRequestTest, Case1) {
-  ft::HTTPHead req;
-  std::string     line = "GET http://example.com/private/index.html HTTP/1.1\r\nhost: hostname\r\n\r\n";
+    TEST_F(HTTPRequestTest, CaseGetURL) {
+        ft::HTTPHead req;
+        std::string line = "GET http://example.com/private/index.html HTTP/1.1\r\nhost: hostname\r\n\r\n";
 
-  int ret = req.Parse(std::string(line));
-  EXPECT_EQ(req.GetRequestMethod(), "GET");
-  EXPECT_EQ(req.GetRequestURI(), "http://example.com/private/index.html");
-  std::cout << "Host:           " << req.GetHost() << std::endl;
-  std::cout << "ResponseCode:   " << req.GetResponseCode() << std::endl;
-  std::cout << "ParseStatus:    " << req.GetParseStatus() << std::endl;
-  std::cout << "RequestMethod:  " << req.GetRequestMethod() << std::endl;
-  std::cout << "RequestURI:     " << req.GetRequestURI() << std::endl;
-  std::cout << "HTTPv:          " << req.GetHTTPv() << std::endl;
-//  std::cout << "HeaderFields:   " << req.GetHeaderFields() << std::endl;
-  std::cout << "Save:           " << req.getSave() << std::endl;
-}
+        int ret = req.Parse(line);
+        EXPECT_EQ(ret, 0);
+        EXPECT_EQ(req.GetRequestMethod(), "GET");
+        EXPECT_EQ(req.GetRequestURI(), "http://example.com/private/index.html");
+        EXPECT_EQ(req.GetHost(), "hostname");
+        req.ParseRequestURI();
+        EXPECT_EQ(req.GetHost(), "example.com");
+        std::cout << "host: " << req.GetHost() << std::endl;
+        EXPECT_EQ(req.GetRequestURI(), "/private/index.html");
+        EXPECT_EQ(req.GetResponseCode(), 200);
+        std::cout << "RC: " << req.GetResponseCode() << std::endl;
+        EXPECT_EQ(req.GetParseStatus(), ft::complete);
+        EXPECT_EQ(req.GetHTTPv(), "HTTP/1.1");
+        EXPECT_EQ(req.GetHeaderFields().find("host")->second, "hostname");
+    }
 
-TEST_F(HTTPRequestTest, CGICase) {
-  ft::HTTPHead req;
-  std::string  line = "GET http://example.com/cgi-bin/script.cgi?a=100&b=200 HTTP/1.1\r\nhost: hostname\r\n\r\n";
+    TEST_F(HTTPRequestTest, CaseGetURI) {
+        ft::HTTPHead req;
+        std::string line = "GET /private/index.html HTTP/1.1\r\nhost: hostname\r\n\r\n";
 
-  int ret = req.Parse(std::string(line));
-  std::cout << "Host:           " << req.GetHost() << std::endl;
-  std::cout << "ResponseCode:   " << req.GetResponseCode() << std::endl;
-  std::cout << "ParseStatus:    " << req.GetParseStatus() << std::endl;
-  std::cout << "RequestMethod:  " << req.GetRequestMethod() << std::endl;
-  std::cout << "RequestURI:     " << req.GetRequestURI() << std::endl;
-  std::cout << "HTTPv:          " << req.GetHTTPv() << std::endl;
-//  std::cout << "HeaderFields:   " << req.GetHeaderFields() << std::endl;
-  std::cout << "Save:           " << req.getSave() << std::endl;
-}
+        int ret = req.Parse(std::string(line));
+        EXPECT_EQ(ret, 0);
+        EXPECT_EQ(req.GetRequestMethod(), "GET");
+        EXPECT_EQ(req.GetRequestURI(), "/private/index.html");
+        EXPECT_EQ(req.GetHost(), "hostname");
+        req.ParseRequestURI();
+        EXPECT_EQ(req.GetHost(), "hostname");
+        EXPECT_EQ(req.GetRequestURI(), "/private/index.html");
+        EXPECT_EQ(req.GetResponseCode(), 200);
+        EXPECT_EQ(req.GetParseStatus(), ft::complete);
+        EXPECT_EQ(req.GetHTTPv(), "HTTP/1.1");
+        EXPECT_EQ(req.GetHeaderFields().find("host")->second, "hostname");
+    }
+
+    TEST_F(HTTPRequestTest, CaseCGI) {
+        ft::HTTPHead req;
+        std::string line = "GET /cgi-bin/script.cgi/?10+20 HTTP/1.1\r\nhost:hostname\r\n\r\n";
+
+        int ret = req.Parse(std::string(line));
+        EXPECT_EQ(ret, 0);
+        EXPECT_EQ(req.GetRequestMethod(), "GET");
+        EXPECT_EQ(req.GetRequestURI(), "/cgi-bin/script.cgi/?10+20");
+        EXPECT_EQ(req.GetHost(), "hostname");
+        req.ParseRequestURI();
+        EXPECT_EQ(req.GetHost(), "hostname");
+        EXPECT_EQ(req.GetRequestURI(), "/cgi-bin/script.cgi/?10+20");
+        EXPECT_EQ(req.GetResponseCode(), 200);
+        EXPECT_EQ(req.GetParseStatus(), ft::complete);
+        EXPECT_EQ(req.GetHTTPv(), "HTTP/1.1");
+        EXPECT_EQ(req.GetHeaderFields().find("host")->second, "hostname");
+    }
+
+    TEST_F(HTTPRequestTest, CasePOST) {
+        ft::HTTPHead req;
+        std::string line = "POST / HTTP/1.1\r\nhost:hostname\r\ncontent-length:5\r\n\r\n";
+        std::string body = "hey\r\n";
+
+        int ret = req.Parse(std::string(line + body));
+        EXPECT_EQ(ret, 0);
+        EXPECT_EQ(req.GetRequestMethod(), "POST");
+        EXPECT_EQ(req.GetRequestURI(), "/");
+        EXPECT_EQ(req.GetHost(), "hostname");
+        EXPECT_EQ(req.GetResponseCode(), 200);
+        EXPECT_EQ(req.GetParseStatus(), ft::complete);
+        EXPECT_EQ(req.GetHTTPv(), "HTTP/1.1");
+        EXPECT_EQ(req.GetHeaderFields().find("host")->second, "hostname");
+        EXPECT_EQ(req.GetHeaderFields().find("content-length")->second, "5");
+        EXPECT_EQ(req.getSave(), body);
+    }
 }
