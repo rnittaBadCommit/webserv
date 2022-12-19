3b19e0ad0985c4b295d2fecfea378cffbe6d7b29
ymori <yuu.mori06@gmail.com>
2022-12-19 17:23:28 +0900

    #16 #27 add http_process in server.cpp !!
    I don't know if it works.

--- srcs/httpMethod/HTTPProcess.hpp
+++ srcs/httpMethod/HTTPProcess.hpp
@@ -5,4 +5,8 @@
 #ifndef WEBSERV_SRCS_HTTPMETHOD_HTTPPROCESS_HPP_
 #define WEBSERV_SRCS_HTTPMETHOD_HTTPPROCESS_HPP_
 
+#include "HTTPProcess.hpp"
+
+std::string http_process(ft::ServerChild server_child);
+
 #endif //WEBSERV_SRCS_HTTPMETHOD_HTTPPROCESS_HPP_
